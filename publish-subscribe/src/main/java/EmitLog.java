3edc19e3c219467bad56de8c8d8e84216b1d4662
guolong.zha <guolong.zha@mtime.com>
2018-01-23 16:45:43 +0800

    rabbitmq demo

---
+++ publish-subscribe/src/main/java/EmitLog.java
@@ -0,0 +1,30 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Created by ZhaGuolong on 2018/1/23.
+ */
+public class EmitLog {
+
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] argv) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.9.66");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+//      分发消息
+        for(int i = 0 ; i < 5; i++){
+            String message = "Hello World! " + i;
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+        channel.close();
+        connection.close();
+    }
+}
